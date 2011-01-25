b44d640594743f8fd549e920bff0e022f23dc442
KungD <david@krawaller.se>
2011-01-25 10:43:40 +0100

    castle rules

--- src/algol.js
+++ src/algol.js
@@ -311,7 +311,7 @@ Algol = (function(){
 		return ret;
 	}
 	
-	function searchCauldron(cauldron,test,excepting,vars){
+	function searchCauldron(cauldron,test,vars,excepting){
 		var ret = [], list, lists = [], copy = ["x","y"];
 		switch(test.type){
 			case "AND":
@@ -328,7 +328,7 @@ Algol = (function(){
 			ret.push(o);
 		});
 		if (test.except && !excepting){
-			var forbidden = searchCauldron(cauldron,test.except,true), oklist = [];
+			var forbidden = searchCauldron(cauldron,test.except,vars,true), oklist = [];
 			ret.map(function(l){
 				var violating = false;
 				forbidden.map(function(f){
@@ -353,29 +353,29 @@ Algol = (function(){
 					height: 8,
 					width: 8,
 					terrain: [
-						{x:1,y:1,type:"base",plr:1},{x:2,y:1,type:"base",plr:1},{x:3,y:1,type:"base",plr:1},{x:4,y:1,type:"base",plr:1},{x:5,y:1,type:"base",plr:1},{x:6,y:1,type:"base",plr:1},{x:7,y:1,type:"base",plr:1},{x:8,y:1,type:"base",plr:1},
-						{x:1,y:8,type:"base",plr:2},{x:2,y:8,type:"base",plr:2},{x:3,y:8,type:"base",plr:2},{x:4,y:8,type:"base",plr:2},{x:5,y:8,type:"base",plr:2},{x:6,y:8,type:"base",plr:2},{x:7,y:8,type:"base",plr:2},{x:8,y:8,type:"base",plr:2}
+						{x:1,y:1,terrain:"base",plr:1},{x:2,y:1,terrain:"base",plr:1},{x:3,y:1,terrain:"base",plr:1},{x:4,y:1,terrain:"base",plr:1},{x:5,y:1,terrain:"base",plr:1},{x:6,y:1,terrain:"base",plr:1},{x:7,y:1,terrain:"base",plr:1},{x:8,y:1,terrain:"base",plr:1},
+						{x:1,y:8,terrain:"base",plr:2},{x:2,y:8,terrain:"base",plr:2},{x:3,y:8,terrain:"base",plr:2},{x:4,y:8,terrain:"base",plr:2},{x:5,y:8,terrain:"base",plr:2},{x:6,y:8,terrain:"base",plr:2},{x:7,y:8,terrain:"base",plr:2},{x:8,y:8,terrain:"base",plr:2}
 					]
 				}
 			},
 			setups: {
 				standard: {
-					1: {x:4,y:1,plr:1,type:"crown"},
-					2: {x:5,y:1,plr:1,type:"crown"},
-					3: {x:3,y:2,plr:1,type:"dagger"},
-					4: {x:4,y:2,plr:1,type:"dagger"},
-					5: {x:5,y:2,plr:1,type:"dagger"},
-					6: {x:6,y:2,plr:1,type:"dagger"},
-					7: {x:3,y:8,plr:2,type:"crown"},
-					8: {x:6,y:8,plr:2,type:"crown"},
-					9: {x:2,y:7,plr:2,type:"dagger"},
-					10: {x:3,y:7,plr:2,type:"dagger"},
-					11: {x:4,y:7,plr:2,type:"dagger"},
-					12: {x:5,y:7,plr:2,type:"dagger"},
-					13: {x:6,y:7,plr:2,type:"dagger"},
-					14: {x:7,y:7,plr:2,type:"dagger"},
-					15: {x:3,y:6,plr:2,type:"dagger"},
-					16: {x:6,y:6,plr:2,type:"dagger"}
+					1: {x:4,y:1,plr:1,unit:"crown"},
+					2: {x:5,y:1,plr:1,unit:"crown"},
+					3: {x:3,y:2,plr:1,unit:"dagger"},
+					4: {x:4,y:2,plr:1,unit:"dagger"},
+					5: {x:5,y:2,plr:1,unit:"dagger"},
+					6: {x:6,y:2,plr:1,unit:"dagger"},
+					7: {x:3,y:8,plr:2,unit:"crown"},
+					8: {x:6,y:8,plr:2,unit:"crown"},
+					9: {x:2,y:7,plr:2,unit:"dagger"},
+					10: {x:3,y:7,plr:2,unit:"dagger"},
+					11: {x:4,y:7,plr:2,unit:"dagger"},
+					12: {x:5,y:7,plr:2,unit:"dagger"},
+					13: {x:6,y:7,plr:2,unit:"dagger"},
+					14: {x:7,y:7,plr:2,unit:"dagger"},
+					15: {x:3,y:6,plr:2,unit:"dagger"},
+					16: {x:6,y:6,plr:2,unit:"dagger"}
 				}
 			},
 			winconditions: {
@@ -436,7 +436,9 @@ Algol = (function(){
 					actionpoints: 1
 				}
 			},
-			actionpointsperturn: 1,
+			endturnconditions:{
+				actionpointsspent: 1
+			},
 			artifactgenerators: {
 				nexttocrown: {
 					type: "walker",
@@ -526,7 +528,7 @@ Algol = (function(){
 							from: "artifacts",
 							props: {
 								tag: "daggertarget",
-								dir: [2,4,6,8]
+								dir: [1,5]
 							}
 						},{
 							from: "units",
@@ -553,6 +555,229 @@ Algol = (function(){
 							props: {
 								plr: "OPPONENT"
 							}
+						}]
+					}
+				}
+			}
+		},
+		castle: {
+			boards: {
+				standard: {
+					height: 19,
+					width: 19,
+					terrain: [
+						{x:6,y:1,terrain:"castle"},{x:14,y:1,terrain:"castle"},
+						{x:3,y:2,terrain:"castle"},{x:6,y:2,terrain:"castle"},{x:8,y:2,terrain:"castle"},{x:9,y:2,terrain:"castle"},{x:11,y:2,terrain:"castle"},{x:12,y:2,terrain:"castle"},{x:14,y:2,terrain:"castle"},{x:17,y:2,terrain:"castle"},
+						{x:3,y:3,terrain:"castle"},{x:6,y:3,terrain:"castle"},{x:8,y:3,terrain:"castle"},{x:12,y:3,terrain:"castle"},{x:14,y:3,terrain:"castle"},{x:17,y:3,terrain:"castle"},
+						{x:3,y:4,terrain:"castle"},{x:6,y:4,terrain:"castle"},{x:8,y:4,terrain:"castle"},{x:12,y:4,terrain:"castle"},{x:14,y:4,terrain:"castle"},{x:17,y:4,terrain:"castle"},
+						{x:3,y:5,terrain:"castle"},{x:6,y:5,terrain:"castle"},{x:8,y:5,terrain:"castle"},{x:12,y:5,terrain:"castle"},{x:14,y:5,terrain:"castle"},{x:17,y:5,terrain:"castle"},
+						{x:3,y:6,terrain:"castle"},{x:6,y:6,terrain:"castle"},{x:8,y:6,terrain:"castle"},{x:9,y:6,terrain:"castle"},{x:10,y:6,terrain:"castle"},{x:11,y:6,terrain:"castle"},{x:12,y:6,terrain:"castle"},{x:14,y:6,terrain:"castle"},{x:17,y:6,terrain:"castle"},
+						{x:3,y:7,terrain:"castle"},{x:6,y:7,terrain:"castle"},{x:8,y:7,terrain:"castle"},{x:12,y:7,terrain:"castle"},{x:14,y:7,terrain:"castle"},{x:17,y:7,terrain:"castle"},
+						{x:3,y:8,terrain:"castle"},{x:17,y:8,terrain:"castle"},
+						{x:3,y:12,terrain:"castle"},{x:4,y:12,terrain:"castle"},{x:5,y:12,terrain:"castle"},{x:6,y:12,terrain:"castle"},{x:7,y:12,terrain:"castle"},{x:8,y:12,terrain:"castle"},{x:9,y:12,terrain:"castle"},{x:11,y:12,terrain:"castle"},{x:12,y:12,terrain:"castle"},{x:13,y:12,terrain:"castle"},{x:14,y:12,terrain:"castle"},{x:15,y:12,terrain:"castle"},{x:16,y:12,terrain:"castle"},{x:17,y:12,terrain:"castle"},
+						{x:3,y:13,terrain:"castle"},{x:17,y:13,terrain:"castle"},
+						{x:3,y:14,terrain:"castle"},{x:6,y:14,terrain:"castle"},{x:8,y:14,terrain:"castle"},{x:9,y:14,terrain:"castle"},{x:10,y:14,terrain:"castle"},{x:11,y:14,terrain:"castle"},{x:12,y:14,terrain:"castle"},{x:14,y:14,terrain:"castle"},{x:17,y:14,terrain:"castle"},
+						{x:3,y:15,terrain:"castle"},{x:6,y:15,terrain:"castle"},{x:8,y:15,terrain:"castle"},{x:12,y:15,terrain:"castle"},{x:14,y:15,terrain:"castle"},{x:17,y:15,terrain:"castle"},
+						{x:3,y:16,terrain:"castle"},{x:6,y:16,terrain:"castle"},{x:8,y:16,terrain:"castle"},{x:12,y:16,terrain:"castle"},{x:14,y:16,terrain:"castle"},{x:17,y:16,terrain:"castle"},
+						{x:3,y:17,terrain:"castle"},{x:6,y:17,terrain:"castle"},{x:8,y:17,terrain:"castle"},{x:12,y:17,terrain:"castle"},{x:14,y:17,terrain:"castle"},{x:17,y:17,terrain:"castle"},
+						{x:3,y:18,terrain:"castle"},{x:6,y:18,terrain:"castle"},{x:8,y:18,terrain:"castle"},{x:9,y:18,terrain:"castle"},{x:11,y:18,terrain:"castle"},{x:12,y:18,terrain:"castle"},{x:14,y:18,terrain:"castle"},{x:17,y:18,terrain:"castle"},
+						{x:6,y:19,terrain:"castle"},{x:14,y:19,terrain:"castle"},
+						{x:10,y:4,terrain:"throne",plr:1},
+						{x:10,y:16,terrain:"throne",plr:2}
+					]
+				}
+			},
+			setups: {
+				standard: {
+					1: {x:6,y:1,plr:1,unit:"soldier"},
+					2: {x:14,y:1,plr:1,unit:"soldier"},
+					3: {x:8,y:2,plr:1,unit:"soldier"},
+					4: {x:12,y:2,plr:1,unit:"soldier"},
+					5: {x:8,y:6,plr:1,unit:"soldier"},
+					6: {x:12,y:6,plr:1,unit:"soldier"},
+					7: {x:3,y:8,plr:1,unit:"soldier"},
+					8: {x:17,y:8,plr:1,unit:"soldier"},
+					9: {x:6,y:19,plr:2,unit:"soldier"},
+					10: {x:14,y:19,plr:2,unit:"soldier"},
+					11: {x:8,y:18,plr:2,unit:"soldier"},
+					12: {x:12,y:18,plr:2,unit:"soldier"},
+					13: {x:8,y:14,plr:2,unit:"soldier"},
+					14: {x:12,y:14,plr:2,unit:"soldier"},
+					15: {x:3,y:12,plr:2,unit:"soldier"},
+					16: {x:17,y:12,plr:2,unit:"soldier"}
+				}
+			},
+			winconditions: {
+				counts: [{
+					ifnot: 0,
+					test: {
+						type: "AND",
+						tests: [{
+							from: "units",
+							props: {
+								plr: "CURRENTPLAYER"
+							}
+						},{
+							from: "terrain",
+							props: {
+								plr: "OPPONENT",
+								terrain: "throne"
+							}
+						}]
+					}
+				}]
+			},
+			endturnconditions: {
+				actionpointsspent: 1
+			},
+			marks: {
+				select: {
+					clear: true,
+					prio: 1
+				},
+				move: {
+					prio: 2
+				},
+				kill: {
+					prio: 3
+				}
+			},
+			commands: {
+				move: {
+					mark: "move",
+					targetmark: "select",
+					movetomark: "move",
+					actionpoints: 1
+				},
+				kill: {
+					mark: "kill",
+					targetmark: "kill",
+					kill: true,
+					actionpoints: 1
+				}
+			},
+			artifactgenerators: {
+				wallslide: {
+					type: "walker",
+					aid: "wallslide",
+					dirs: [1,3,5,7],
+					createatstop: true,
+					createatstep: true,
+					stoptag: "reach",
+					stepmark: "move",
+					starts: {
+						type: "AND",
+						tests: [{
+							from:"units",
+							props:{plr:"CURRENTPLAYER"},
+							collect: "uid"
+						},{
+							from:"terrain",
+							props: {terrain:"castle"}
+						}]
+					},
+					steps: {
+						from: "terrain",
+						props: {
+							terrain: "castle"
+						}
+					},
+					stops: {
+						from:"units",
+						props:{},
+						collect: "uid"
+					}
+				},
+				groundslide: {
+					type: "walker",
+					aid: "wallslide",
+					dirs: [1,3,5,7],
+					createatstop: true,
+					createatstep: true,
+					stoptag: "reach",
+					stepmark: "move",
+					starts: {
+						type: "AND",
+						tests: [{
+							from:"units",
+							props:{plr:"CURRENTPLAYER"},
+							collect: "uid"
+						},{
+							from:"terrain",
+							props: {terrain:"castle"}
+						}]
+					},
+					stops: {
+						type: "OR",
+						tests: [{
+							from:"units",
+							props:{}
+						},{
+							from:"terrain",
+							props: {}
+						}]
+					}
+				},
+				nexttounit: {
+					type: "offset",
+					aid: "nexttounit",
+					dirs: [1,3,5,7],
+					forward: 1,
+					right: 0,
+					tag: "reach",
+					starts: {
+						type: "PROPS",
+						test: {
+							from:"units",
+							props: {plr:"CURRENTPLAYER"},
+							collect: "uid"
+						}
+					}
+				},
+				forbidden: {
+					type: "spawn",
+					aid: "forbidden",
+					tag: "forbidden",
+					test: {
+						type: "AND",
+						tests: [{
+							from: "units",
+							props: {}
+						},{
+							from: "terrain",
+							props: {
+								terrain: "throne",
+								plr: "CURRENTPLAYER"
+							}
+						}]
+					}
+				},
+				reachmove: {
+					type: "spawn",
+					aid: "reachmove",
+					mark: "move",
+					test: {
+						type: "PROPS",
+						from: "artifacts",
+						props: { tag: "reach" },
+						except: {
+							from: "artifacts",
+							props: {tag:"forbidden"}
+						}
+					}
+				},
+				kill: {
+					type: "spawn",
+					aid: "kill",
+					mark: "kill",
+					test: {
+						type: "AND",
+						tests: [{
+							from: "artifacts",
+							props: { tag: "reach" }
+						},{
+							from: "units",
+							props: { plr: "OPPONENT" }
 						}]
 					}
 				}
